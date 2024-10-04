c3c6bce2f6962555e6185eda732e2ea1b87083e9
Nguyen Chi Dat <nguyenchidat1442001@gmail.com>
2024-10-04 13:57:40 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+
+# Tạo một ứng dụng FastAPI
+app = FastAPI()
+
+# Định nghĩa route để trả về "Hello, World!"
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+# Block if __name__ == "__main__"
+if __name__ == "__main__":
+    # Chạy Uvicorn với host và port tùy chỉnh nếu cần
+    uvicorn.run(app, host="127.0.0.1", port=8000)
